c470c3940c84a0b5d8790193c40213407bc1e2d5
Patrick Flanagan <unrflanagan@gmail.com>
2026-01-02 21:05:27 -0800

    style: Match active filter dropdown to search input style

--- app/dashboard/SearchFilterBar.tsx
+++ app/dashboard/SearchFilterBar.tsx
@@ -42,7 +42,7 @@ export default function SearchFilterBar() {
             </div>
             <div>
                 <select
-                    className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={(e) => handleStatusChange(e.target.value)}
                     defaultValue={searchParams.get('status')?.toString() || 'active'}
                 >
